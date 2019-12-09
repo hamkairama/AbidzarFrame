e8b17e6c54bfd61aaf39f19348330ed36619bb2e
hirama <hamka.irama@protelindo.net>
2019-12-09 17:28:34 +0700

    hamka

---
+++ Source/AbidzarFrame.Web/Areas/Master/Controllers/TestimoniController.cs
@@ -0,0 +1,252 @@
+﻿using AbidzarFrame.Core.Model.Business;
+using AbidzarFrame.Core.Mvc.Helpers;
+using AbidzarFrame.Domain.Common;
+using AbidzarFrame.Domain.Models;
+using AbidzarFrame.Master.Interface.Dto;
+using AbidzarFrame.Mvc.Infrastructures.ActionResult;
+using AbidzarFrame.Utils;
+using AbidzarFrame.Web.Helpers;
+using AbidzarFrame.Web.Models;
+using AbidzarFrame.Web.WebApiLocal;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbidzarFrame.Web.Areas.Master.Controllers
+{
+    public class TestimoniController : WebLogManager
+    {
+
+        #region Private Instance
+        private FunctionLog functionLog = new FunctionLog();
+        private const string controller = "TestimoniController";
+        private string token = AuthenticationConfiguration.GetInstance().WebServiceAuthenticationToken;
+        private ResultStatus result = new ResultStatus();
+        BusinessErrors bussinessError = new BusinessErrors();
+        TestimoniRequest request = new TestimoniRequest();
+        TestimoniResponse response = new TestimoniResponse();
+        protected ErrorHandler _errHand
+        {
+            get { return new ErrorHandler(); }
+        }
+        #endregion
+
+        #region Contructor 
+        public TestimoniController()
+        {
+        }
+        #endregion
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Read()
+        {
+            try
+            {
+                response = GetDataList();
+                var data = response.TestimoniResultList;
+                return Json(new { data = data.ToList() }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return JsonError(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public TestimoniResponse GetDataList()
+        {
+            string method = MethodBase.GetCurrentMethod().Name;
+            string apiUrl = "api/Testimoni/GetTestimoniList";
+
+            try
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.begin);
+
+                request.AuthenticationToken = token;
+                //call api
+                response = JsonConvert.DeserializeObject<TestimoniResponse>(JasonMapper.ConvertHttpResponseToJson(apiUrl, request));
+            }
+            catch (Exception ex)
+            {
+                _errHand.FillError(ex.Message, ref bussinessError);
+            }
+            finally
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.end);
+            }
+
+            return response;
+
+        }
+
+        public ActionResult EditorFormPartial(int id)
+        {
+            string method = MethodBase.GetCurrentMethod().Name;
+            string apiUrl = "api/Testimoni/GetTestimoniFindBy";
+
+            try
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.begin);
+
+                request.AuthenticationToken = token;
+                request.Id = id;
+                //call api if not adding / create
+                if (id > 0)
+                {
+                    response = JsonConvert.DeserializeObject<TestimoniResponse>(JasonMapper.ConvertHttpResponseToJson(apiUrl, request));
+                }
+            }
+            catch (Exception ex)
+            {
+                _errHand.FillError(ex.Message, ref bussinessError);
+                return JsonError(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+            finally
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.end);
+            }
+
+            if (id > 0)
+            {
+                ViewBag.ActionGrid = "Edit";
+            }
+            else
+            {
+                ViewBag.ActionGrid = "Add";
+            }
+            
+            return PartialView("_Testimoni", response.TestimoniResult);
+        }
+
+        public ActionResult Create(TestimoniRequest model)
+        {
+            string method = MethodBase.GetCurrentMethod().Name;
+            string apiUrl = "api/Testimoni/InsertTestimoni";
+
+            try
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.begin);
+
+                //set id is null 
+                model.Id = 0;
+                if (!ModelState.IsValid)
+                {
+                    var errors = new Hashtable();
+                    foreach (var pair in ModelState)
+                    {
+                        if (pair.Value.Errors.Count > 0)
+                            errors[pair.Key] = pair.Value.Errors.Select(error => error.ErrorMessage).ToList();
+                    }
+                    return Json(new { success = false, errors }, JsonRequestBehavior.AllowGet);
+                }
+
+                request = model;
+                request.AuthenticationToken = token;
+
+                //call api
+                request.DibuatOleh = CurrentUser.GetCurrentUserId();
+                response = JsonConvert.DeserializeObject<TestimoniResponse>(JasonMapper.ConvertHttpResponseToJson(apiUrl, request));
+                if (!response.ResultStatus.IsSuccess)
+                {
+                    return JsonError(response.ResultStatus.MessageText, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                _errHand.FillError(ex.Message, ref bussinessError);
+                return JsonError(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+            finally
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.end);
+            }
+
+            return Json(new { success = true, Msg = "Success Insert Data" }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Update(TestimoniRequest model)
+        {
+            string method = MethodBase.GetCurrentMethod().Name;
+            string apiUrl = "api/Testimoni/UpdateTestimoni";
+
+            try
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.begin);
+
+                if (string.IsNullOrWhiteSpace(model.IdRw.ToString()))
+                {
+                    throw new InvalidOperationException(_errHand.GetErrorMessage(ErrorHandler._ERRKEY_ABIDZARFRAME, Utils.ErrorMessages.ABIDZARFRAME_DATA_NOT_FOUND_CD));
+                }
+                if (!ModelState.IsValid)
+                {
+                    var errors = new Hashtable();
+                    foreach (var pair in ModelState)
+                    {
+                        if (pair.Value.Errors.Count > 0)
+                            errors[pair.Key] = pair.Value.Errors.Select(error => error.ErrorMessage).ToList();
+                    }
+                    return Json(new { success = false, errors }, JsonRequestBehavior.AllowGet);
+                }
+
+                request = model;
+                request.AuthenticationToken = token;
+                request.DieditOleh = CurrentUser.GetCurrentUserId();
+                //call api
+                response = JsonConvert.DeserializeObject<TestimoniResponse>(JasonMapper.ConvertHttpResponseToJson(apiUrl, request));
+                if (!response.ResultStatus.IsSuccess)
+                {
+                    return JsonError(response.ResultStatus.MessageText, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                return JsonError(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+            finally
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.end);
+            }
+
+
+            return Json(new { success = true, Msg = "Success Update Data" }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            string method = MethodBase.GetCurrentMethod().Name;
+            string apiUrl = "api/Testimoni/DeleteTestimoni";
+
+            try
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.begin);
+
+                request.AuthenticationToken = token;
+                request.Id = id;
+                //call api
+                response = JsonConvert.DeserializeObject<TestimoniResponse>(JasonMapper.ConvertHttpResponseToJson(apiUrl, request));
+            }
+            catch (Exception ex)
+            {
+                return JsonError(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+            finally
+            {
+                WriteFunctionLog(controller, method, 1, LogData.LOG_TYPE.end);
+            }
+
+
+            return Json(new { success = true, Msg = "Success Delete Data" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
